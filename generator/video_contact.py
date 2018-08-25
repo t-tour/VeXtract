3c5fb4ccc4b6a40079ffad24fcbed66a88aed1ed
RainingNight <IamJohn0329@gmail.com>
2018-08-25 22:12:26 +0800

    新增video_contact的功能

---
+++ generator/video_contact.py
@@ -0,0 +1,34 @@
+import subprocess
+import re
+import math
+import os
+from optparse import OptionParser
+
+
+def contact_by_type(video_type, output_type):
+    contact_cmd = "(for %i in (*." + video_type + \
+        ") do @echo file '%i') > mylist.txt"
+    print("########################################################")
+    print("About to run: "+contact_cmd)
+    print("########################################################")
+    output = subprocess.Popen(contact_cmd, shell=True,
+                              stdout=subprocess.PIPE).stdout.read()
+    contact_cmd = "ffmpeg -f concat -i mylist.txt -c copy output."+output_type
+    print("########################################################")
+    print("About to run: "+contact_cmd)
+    print("########################################################")
+    output = subprocess.Popen(contact_cmd, shell=True,
+                              stdout=subprocess.PIPE).stdout.read()
+
+def contact_by_manifest(output_type):
+    contact_cmd = "ffmpeg -f concat -i mylist.txt -c copy output."+output_type
+    print("########################################################")
+    print("About to run: "+contact_cmd)
+    print("########################################################")
+    output = subprocess.Popen(contact_cmd, shell=True,
+                              stdout=subprocess.PIPE).stdout.read()
+
+if __name__ == '__main__':
+    video_type = input("請輸入要合併的影片類型：")
+    output_type = input("請輸入要輸出的影片類型：")
+    contact_by_type(video_type, output_type)
