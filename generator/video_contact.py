f6ee0d4ee115f41e31c06f71af2fba09b2fe1a88
RainingNight <IamJohn0329@gmail.com>
2018-08-30 18:06:47 +0800

    把print改成log.i，並且會把output.mp4 移到file資料夾

--- generator/video_contact.py
+++ generator/video_contact.py
@@ -1,32 +1,47 @@
+import os
+import sys
+__root = os.path.abspath(
+    os.path.dirname(os.path.abspath(__file__)) + (os.sep + '..') * (
+        len(os.path.dirname(os.path.abspath(__file__)).split(os.sep)) -
+        os.path.dirname(os.path.abspath(__file__)).split(os.sep).index(
+            'VeXtract'
+        ) - 1
+    )) + os.sep
+sys.path.append(__root)
+from helper import logger
+log = logger.Logger(__name__)
 import subprocess
 import re
 import math
-import os
 from optparse import OptionParser
 
 
 def contact_by_type(video_type, output_type, cmd_extra_code=""):
     contact_cmd = "(for %i in (*." + video_type + \
         ") do @echo file '%i') > mylist.txt"
-    print("########################################################")
-    print("About to run: "+cmd_extra_code + contact_cmd)
-    print("########################################################")
+    log.i("########################################################")
+    log.i("About to run: "+cmd_extra_code + contact_cmd)
+    log.i("########################################################")
     subprocess.Popen(cmd_extra_code+contact_cmd, shell=True,
                      stdout=subprocess.PIPE).stdout.read()
     contact_cmd = "ffmpeg -f concat -i mylist.txt -c copy output."+output_type
-    print("########################################################")
-    print("About to run: "+cmd_extra_code + contact_cmd)
-    print("########################################################")
+    log.i("########################################################")
+    log.i("About to run: "+cmd_extra_code + contact_cmd)
+    log.i("########################################################")
     subprocess.Popen(cmd_extra_code+contact_cmd, shell=True,
+                     stdout=subprocess.PIPE).stdout.read()
+    subprocess.Popen("move output.mp4 "+os.path.join(__root, "file"), shell=True,
                      stdout=subprocess.PIPE).stdout.read()
 
 
 def contact_by_manifest(output_type):
     contact_cmd = "ffmpeg -f concat -i mylist.txt -c copy output."+output_type
-    print("########################################################")
-    print("About to run: "+contact_cmd)
-    print("########################################################")
+    log.i("########################################################")
+    log.i("About to run: "+contact_cmd)
+    log.i("########################################################")
     subprocess.Popen(contact_cmd, shell=True,
+                     stdout=subprocess.PIPE).stdout.read()
+    subprocess.Popen("move output.mp4 "+os.path.join(__root, "file"), shell=True,
                      stdout=subprocess.PIPE).stdout.read()
 
 
