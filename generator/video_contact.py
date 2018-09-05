5b789aead95bc0a72b72bfc570b1ffaae19a0703
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-09-05 17:55:17 +0800

    contact_by_manifest function 修正 - 路徑包含 space 問題 - 輸出信息扔了 - ffmepg concat 改成 forece

--- generator/video_contact.py
+++ generator/video_contact.py
@@ -18,6 +18,7 @@ from optparse import OptionParser
 
 def contact_by_type(video_type, output_type, output_name="output", cmd_extra_code=""):
     # FIXME: 以後可能會架在 linux 上面運行 所以需要跨平台的指令
+    # FIXME: 沒有修過喔~
     contact_cmd = "(for %i in (*." + video_type + \
         ") do @echo file '%i') > mylist.txt"
     log.i("########################################################")
@@ -35,25 +36,35 @@ def contact_by_type(video_type, output_type, output_name="output", cmd_extra_cod
                      stdout=subprocess.PIPE).stdout.read()
 
 
-def contact_by_manifest(video_tuple, output_type, output_name="output"):
+def contact_by_manifest(video_tuple, output_name="output"):
     # FIXME: 以後可能會架在 linux 上面運行 所以需要跨平台的指令
+    defalut_ext = video_tuple[0].split(".").pop()
+    prefer_ext = output_name.split(".").pop()
+    if prefer_ext != defalut_ext:
+        log.i('input format is {}  and your output format is {}'.format(
+            defalut_ext, prefer_ext))
     for i in video_tuple:
         contact_cmd = "echo file '{0}'>>mylist.txt".format(i)
         subprocess.Popen(contact_cmd, shell=True,
                          stdout=subprocess.PIPE).stdout.read()
-    contact_cmd = "ffmpeg -f concat -safe 0 -i mylist.txt -c copy " + \
-        output_name + "." + output_type
+    contact_cmd = "ffmpeg -y -f concat -safe 0 -i mylist.txt -c copy \"{}\"".format(
+        output_name)
     log.i("########################################################")
     log.i("About to run: " + contact_cmd)
     log.i("########################################################")
-    subprocess.Popen(contact_cmd, shell=True,
-                     stdout=subprocess.PIPE).stdout.read()
-    subprocess.Popen("del mylist.txt &move "+output_name+"."+output_type+" " +
-                     os.path.join(__root, "file"), shell=True, stdout=subprocess.PIPE).stdout.read()
+    subprocess.Popen(contact_cmd, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    subprocess.Popen("del mylist.txt", shell=True, stdout=subprocess.PIPE).stdout.read()
 
 
 if __name__ == '__main__':
     # 測試用
-    video_tuple = ("03-0.mp4",
-                   "03-1.mp4", "03-2.mp4")
-    contact_by_manifest(video_tuple, "mp4")
+
+    video_tuple = [__root + "file\\crawler\\av30285383\\1_52826704\\" + i
+                   for i in os.listdir(__root + "file\\crawler\\av30285383\\1_52826704\\")]
+    video_tuple = ("52826704-part0.flv", "52826704-part1.flv")
+    video_tuple = tuple(video_tuple)
+
+    print(video_tuple)
+    contact_by_manifest(video_tuple,
+                        output_name=__root + "file/generator/output.mp4")
