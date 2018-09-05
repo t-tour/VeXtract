6a3e34f952cee3743773fc36cac9720c73ae7fc3
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-09-05 14:31:44 +0800

    修正 - log 格式 - fps 小數點 - ffmpeg function call 路徑包含空格 - pep8 - 位數至小數點下 2 位 - import 格式

--- analyzer/algorithm/video_algorithm.py
+++ analyzer/algorithm/video_algorithm.py
@@ -1,4 +1,3 @@
-import subprocess
 import os
 import sys
 __root = os.path.abspath(
@@ -9,32 +8,38 @@ __root = os.path.abspath(
         ) - 1
     )) + os.sep
 sys.path.append(__root)
+
 from helper import logger
 log = logger.Logger(__name__)
 
+import subprocess
+import re
+
 
 def get_video_length(filename):
-    ouput = subprocess.Popen("ffmpeg -i "+filename, stdout=subprocess.PIPE,
+    ouput = subprocess.Popen("ffmpeg -i \"{}\"".format(filename), stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     _, stderr = ouput.communicate()
-    stderr = str(stderr)
-    start = stderr.index("Duration")
-    time = stderr[start+10:start+18].split(":")
-    video_length = int(time[0])*3600 + int(time[1])*60 + int(time[2])
-    log.i("影片長度為："+video_length+"秒")
+    time = re.search(r"Duration: (\d+):(\d+):(\d+\.\d+)",
+                     str(stderr, encoding='utf-8'))
+    log.i('time length: {}:{}:{}'.format(
+        time.group(1), time.group(2), time.group(3)))
+    video_length = int(time.group(1))*3600 + \
+        int(time.group(2))*60 + float(time.group(3))
+    log.i("影片長度為：{:.2f}秒".format(video_length))
     return video_length
 
+
 def get_video_fps(filename):
-    ouput = subprocess.Popen("ffmpeg -i "+filename, stdout=subprocess.PIPE,
+    ouput = subprocess.Popen("ffmpeg -i \"{}\"".format(filename), stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     _, stderr = ouput.communicate()
-    stderr = str(stderr)
-    start = stderr.index("fps")-4
-    video_fps=int(stderr[start:start+3])
-    log.i("影片fps為："+str(video_fps))
+    matcher = re.search(r", ([\d|\.]+) fps", str(stderr, encoding='utf-8'))
+    video_fps = float(matcher.group(1))
+    log.i("影片fps為：{:.2f}fps".format(video_fps))
     return video_fps
 
-if __name__=="__main__":
-    #測試用
-    filename="F:\Git\VeXtract\generator\\03.mp4"
-    get_video_fps(filename)
+
+if __name__ == "__main__":
+    # 測試用
+    get_video_fps(__root + "file\\algorithm\\123.mp4")
