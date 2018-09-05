9fe89344a6ec73234bbe0aeea73c681f06146523
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-09-05 23:19:00 +0800

    修改 import, bilibili comment ,test 更新 testfile 的json

--- test/bilibili_test.py
+++ test/bilibili_test.py
@@ -16,23 +16,8 @@ from crawler import bilibili
 from helper import logger
 log = logger.Logger(__name__)
 
-AV_NUMBER_ERROR_FORMATTING = "aa12345666d"
-AV_NUMBER_NULL = "av2"
 AV_NUMBER_ONE_P = "av23315808"
 AV_NUMBER_MANY_P = "av13392824"
-CID = '38842914'
-
-
-def test_fetch_bilibili_with_error_formatting():
-    log.d('start test_fetch_bilibili_with_error_formatting')
-    with pytest.raises(Exception, match=r'av號格式錯誤'):
-        bilibili.fetch_bilibili_av(AV_NUMBER_ERROR_FORMATTING)
-
-
-def test_fetch_bilibili_with_not_found_error():
-    log.d('start test_fetch_bilibili_with_not_found_error')
-    with pytest.raises(Exception, match=r'.*40[34].*'):
-        bilibili.fetch_bilibili_av(AV_NUMBER_NULL)
 
 
 def test_fetch_bilibili():
@@ -47,15 +32,14 @@ def test_fetch_bilibili():
     assert target.timelength == 110419
 
 
-def test_j_data_rw_and_score_analyzer():
-    log.d('start test_j_data_rw_and_score_analyzer')
-    os.chdir(__root + "test/test_file/")
-    a = bilibili.Bilibili_file_info.load("{}.json".format(AV_NUMBER_ONE_P))
-    assert a.comments[a.cid[0]][0].score == None
-    a.fetch_comment_score(limitation=5000)
-    assert a.comments[a.cid[0]][0].score == 10
-    a.save()
-    b = bilibili.Bilibili_file_info.load("{}.json".format(AV_NUMBER_ONE_P))
-    for comment in b.comments[a.cid[0]]:
-        comment.score = None
-    b.save()
+def test_j_data_rw():
+    log.d('start test_j_data_rw')
+    a = bilibili.Bilibili_file_info.load(os.path.join(__root, "test\\test_file\\{}.json".format(AV_NUMBER_ONE_P)))
+    assert a.comments[a.cid[0]][0]["score"] == None
+    a.comments[a.cid[0]][0]["score"] = 10
+    a.save(os.path.join(__root, "test\\test_file\\"))
+    b = bilibili.Bilibili_file_info.load(os.path.join(__root, "test\\test_file\\{}.json".format(AV_NUMBER_ONE_P)))
+    assert b.comments[b.cid[0]][0]["score"] == 10
+    b.comments[b.cid[0]][0]["score"] = None
+    b.save(os.path.join(__root, "test\\test_file\\"))
+    
