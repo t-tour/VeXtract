80c5a1c394c3d35e8395849a751db73b9bda4328
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-09-13 13:10:48 +0800

    test_wanted_length not complete

--- test/analyzer/algorithm/time_tagger_test.py
+++ test/analyzer/algorithm/time_tagger_test.py
@@ -14,8 +14,13 @@ log = logger.Logger(__name__)
 
 from analyzer.algorithm import time_tagger
 from analyzer.algorithm import video_algorithm
+from crawler.bilibili import bilibili_info
 
-VIDEO = os.path.join(__root, "test\\test_file\\analyzer_file.mp4")
+AVNUMBER = "av23315808"
+AVJSON_PATH = os.path.join(
+    __root, "test\\test_file\\{av}.json".format(av=AVNUMBER))
+VIDEO = os.path.join(__root, "test\\test_file\\{av}.mp4".format(av=AVNUMBER))
+
 
 def test__generate_segments():
     log.i('Strat test__generate_segments.')
@@ -25,3 +30,15 @@ def test__generate_segments():
     for time in a:
         total_length += time[1] - time[0]
     assert assert_total_length == total_length
+
+
+def test_wanted_length():
+    log.i('Start test_wanted_length.')
+    a = bilibili_info.Bilibili_file_info.load(AVJSON_PATH)
+    asserted_total_length = 15.5
+    tagged_list = time_tagger.wanted_length(
+        asserted_total_length, VIDEO, real_time_comments=a.comments[a.cid[0]])
+    total_length = 0
+    for tagged in tagged_list:
+        total_length += tagged[1] - tagged[0]
+    assert asserted_total_length >= total_length
