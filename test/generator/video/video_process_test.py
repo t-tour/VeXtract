4591ef4210c059e926cdabcd33530df5e2d68dff
RainingNight <IamJohn0329@gmail.com>
2018-09-14 17:58:29 +0800

    更改 test_video_process_with_temp的assert寫法

--- test/generator/video/video_process_test.py
+++ test/generator/video/video_process_test.py
@@ -18,22 +18,36 @@ import pytest
 
 from generator.video import video_process
 
-filename = os.path.join(__root, "file", "14391479.flv")
+filename = os.path.join(__root, "file", "03.mp4")
 output_location = os.path.join(__root, "file", "generator")
-output_name = "666.flv"
+output_name = "666.mp4"
 ouput = os.path.join(output_location, output_name)
 
 
 def test_video_process_with_temp():
     log.i('start video_process_with_temp_test.')
+    """
     split_list = [(115.0, 120.0), (425.0, 430.0), (750.0, 755.0), (755.0, 760.0),
                   (760.0, 765.0), (830.0, 835.0), (930.0, 935.0), (985.0, 990.0),
                   (990.0, 995.0), (1135.0, 1140.0), (1140.0, 1145.0), (1585.0, 1590.0)]
+    """
+    """
+    split_list = [(425.0, 430.0), (750.0, 755.0),
+                  (760.0, 765.0), (830.0, 835.0), (930.0, 935.0),
+                  (990.0, 995.0), (1140.0, 1145.0)]
+    """
+    split_list = [(0.0, 5.0), (15.0, 20.0), (45.0, 50.0), (55.0, 60.0),
+                  (75.0, 80.0), (85.0, 90.0), (100.0, 105.0), (115.0, 120.0),
+                  (135.0, 140.0), (155.0, 160.0), (175.0, 180.0), (195.0, 200.0)]
     video_process.video_process(
         filename, split_list, True, output_location, output_name)
-    ouput_temp = os.path.join(output_location, "temp")
     assert os.path.exists(ouput) == True
-    assert os.path.exists(ouput_temp) == True
+    video_name = os.path.basename(filename).split(".")[0]
+    video_type = os.path.basename(filename).split(".")[-1]
+    for i in range(len(split_list)):
+        ouput_temp = os.path.join(
+            output_location, "temp", video_name+"-"+str(i)+"."+video_type)
+        assert os.path.exists(ouput_temp) == True
 
 
 def test_video_process_without_temp():
