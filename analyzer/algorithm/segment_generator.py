9d0ccae37aec7f5af035d5e7ea728ce46a6c88fe
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-11-07 19:47:55 +0800

    更改segment_generator算法及測試案例

--- analyzer/algorithm/segment_generator.py
+++ analyzer/algorithm/segment_generator.py
@@ -12,70 +12,109 @@ sys.path.append(__root)
 from helper import logger
 log = logger.Logger(__name__)
 
+import math
+
 from generator.video import video_process
 from analyzer.audio import dvpt_audio_analyze
 
 
-def generate_segments(video, minimum_length=5000, vocal_frequency_range=(125, 400), trigger=0.3, stability="Not Use Now"):
+MINIMUM_LENGTH = 5000
+VOCAL_FREQUENCY_REANGE = (125, 400)
+TRIGGER = 0.3
+TRIGGER_MULTIPLE = 2
+
+
+def generate_segments(video_or_audio, minimum_length=MINIMUM_LENGTH, vocal_frequency_range=VOCAL_FREQUENCY_REANGE, percentage=TRIGGER, trigger_multiple=TRIGGER_MULTIPLE, stability="Not Use Now"):
     """
     video:影片
     minimum_length:最小長度 milisecond
     vocal_frequency_range:人聲判定範圍
-    trigger:觸發人聲平均
+    trigger:無人聲準位
+    trigger_times:觸發的平均倍數水準
     stability:防止彈跳
     """
-    log.i('Start generate_segments with {}'.format(video))
+    log.i('Start generate_segments with {}'.format(video_or_audio))
 
     # video轉聲訊
-    video_name = "".join(video.split(os.sep)[-1].split(".")[0:-1])
-    video_process.video_encoding(video, os.path.join(
+    video_name = "".join(video_or_audio.split(os.sep)[-1].split(".")[0:-1])
+    video_process.video_encoding(video_or_audio, os.path.join(
         __root, "file", "generator"), video_name + ".wav", ifMain=False)
-    video_audio = os.path.join(
+    video_audio_path = os.path.join(
         __root, "file", "generator", video_name + ".wav")
 
-    spectrums = dvpt_audio_analyze.analyze_audio_list(video_audio)
+    spectrums = dvpt_audio_analyze.analyze_audio_list(
+        video_audio_path, frame_size=48000)
 
-    # 獲取不會觸發trigger的聲音平均強度
-    hz_avg_strength_list = list()
-    avg_strength = float()
-    for spectrum in spectrums:
-        hz_avg_strength_list.append(
-            _segment_strength(spectrum["spectrum"], vocal_frequency_range))
-    hz_avg_strength_list.sort()
-    thirty_percent = int(len(hz_avg_strength_list) * trigger)
-    avg_strength = sum(
-        hz_avg_strength_list[0:thirty_percent]) / float(thirty_percent)
+    avg_strength = _avg_segments_strength(
+        spectrums, vocal_frequency_range, percentage)
 
     # 區分高於平均與否的segments
     above_avg = list()
     below_avg = list()
     for spectrum in spectrums:
-        if _segment_strength(spectrum["spectrum"], vocal_frequency_range) >= avg_strength:
+        if _segment_strength(spectrum["spectrum"], vocal_frequency_range) >= avg_strength * trigger_multiple:
             above_avg.append(spectrum["time"])
         else:
             below_avg.append(spectrum["time"])
 
-    segments_list = _tiny_segments_concat(
-        above_avg) + _tiny_segments_concat(below_avg)
+    segments_list = _segments_concat(
+        above_avg) + _segments_concat(below_avg)
     segments_list.sort(key=lambda foo: foo[0])
 
-    log.i('sements_list is: {}'.format(segments_list))
-    return segments_list
+    # segments過短合併
+    minimum_concated_segments_list = segments_list_minimum_base_concat(
+        segments_list, minimum_length)
+    return minimum_concated_segments_list
 
 
-def _segment_strength(segment, vocal_frequency_range):
-    vocal_hz_strength = list()
-    for hz_strength in segment:
-        if hz_strength[0] > vocal_frequency_range[0]:
-            vocal_hz_strength.append(abs(hz_strength[1]))
-            if not hz_strength[0] < vocal_frequency_range[1]:
-                vocal_hz_strength.pop()
-                return sum(vocal_hz_strength) / float(len(vocal_hz_strength))
+def segments_list_minimum_base_concat(segments_list, minimum_length):
+    selected_segments_list = list()
+    new_segments_list = list()
+    for selected_segment in segments_list:
+        selected_segments_list.append(selected_segment)
+        selected_segments_list = _segments_concat(selected_segments_list)
+        if (selected_segments_list[0][1] - selected_segments_list[0][0]) * 1000 < minimum_length:
+            continue
+        else:
+            new_segments_list.append(selected_segments_list.pop())
+    return new_segments_list
 
 
-def _tiny_segments_concat(segments: list):
+def _avg_segments_strength(spectrums, vocal_frequency_range, percentage):
+    hz_strength_list = list()
+    for spectrum in spectrums:
+        hz_strength_list.append(
+            _segment_strength(spectrum["spectrum"], vocal_frequency_range))
+    hz_strength_list.sort()
+    thirty_percent = int(len(hz_strength_list) * percentage)
+    avg_strength = sum(
+        hz_strength_list[0:thirty_percent]) / float(thirty_percent)
+    log.i('Avg_strength: {}'.format(avg_strength))
+    return avg_strength
+
+
+def _segment_strength(segment, appoint_vocal_frequency_range):
+    segment_frequency_interval = segment[1][0]
+    segment_frequency_limitation = segment[len(segment) - 1][0]
+    if appoint_vocal_frequency_range[0] < 0:
+        raise Exception("appoint_vocal_frequency_range out of bound.")
+    if appoint_vocal_frequency_range[1] > segment_frequency_limitation:
+        raise Exception("appoint_vocal_frequency_range out of bound.")
+    slice_segment_start_section = appoint_vocal_frequency_range[0] / \
+        segment_frequency_interval
+    slice_segment_end_section = appoint_vocal_frequency_range[1] / \
+        segment_frequency_interval
+    strength_list = list()
+    for appoint_frequency in segment[math.ceil(slice_segment_start_section):math.ceil(slice_segment_end_section)]:
+        strength_list.append(abs(appoint_frequency[1]))
+    return sum(strength_list)
+
+
+def _segments_concat(segments: list):
     if len(segments) == 0:
         return []
+    elif len(segments) == 1:
+        return segments
 
     concated_segments = list()
 
--- test/analyzer/algorithm/segment_generator_test.py
+++ test/analyzer/algorithm/segment_generator_test.py
@@ -14,14 +14,39 @@ log = logger.Logger(__name__)
 
 from analyzer.algorithm import segment_generator
 from analyzer.algorithm import video_algorithm
+from analyzer.audio import dvpt_audio_analyze
+from generator.video import video_process
 
 VIDEO = os.path.join(__root, "test", "test_file", "av23315808.mp4")
+VIDEO_AUDIO = os.path.join(
+    __root, "test", "test_file", "Ns_interval_vocal_with_noise.wav")
+SEGMENTS = dvpt_audio_analyze.analyze_audio_list(VIDEO_AUDIO)
+
 
 def test_generate_segments():
     log.i('Strat test_generate_segments.')
-    a = segment_generator.generate_segments(VIDEO)
-    assert_total_length = video_algorithm.get_video_length(VIDEO)
+    a = segment_generator.generate_segments(VIDEO_AUDIO)
+    assert_total_length = video_algorithm.get_video_length(VIDEO_AUDIO)
     total_length = 0
     for time in a:
+        log.d("total={:.4f} start:{:.4f} end:{:.4f}".format(
+            total_length, time[0], time[1]))
         total_length += time[1] - time[0]
-    assert assert_total_length == total_length
+
+    assert assert_total_length >= total_length
+    assert assert_total_length - 0.5 <= total_length
+
+
+def test__segment_strength():
+    log.i('Start test__segment_strength')
+    strength = segment_generator._segment_strength(
+        SEGMENTS[0]['spectrum'], (0, 1))
+    assert strength == abs(
+        SEGMENTS[0]['spectrum'][0][1])
+
+
+def test__segments_concat():
+    test_segments = [(0.5, 2.5), (2.5, 3.7), (4.8, 5.9),
+                     (7.6, 8.4), (8.4, 9.6)]
+    concated_segments = segment_generator._segments_concat(test_segments)
+    assert [(0.5, 3.7), (4.8, 5.9), (7.6, 9.6)] == concated_segments
