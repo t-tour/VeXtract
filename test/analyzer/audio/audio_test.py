7fd4a1f41962a73f57f03032a305f8e3f1a16073
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-12-13 15:11:49 +0800

    更新test項目

--- test/analyzer/audio/audio_test.py
+++ test/analyzer/audio/audio_test.py
@@ -2,8 +2,8 @@ import os
 import sys
 __root = os.path.abspath(
     os.path.dirname(os.path.abspath(__file__)) + (os.sep + '..') * (
-        len(os.path.dirname(os.path.abspath(__file__)).split(os.sep)) -
-        os.path.dirname(os.path.abspath(__file__)).split(os.sep).index(
+        len(os.path.dirname(os.path.abspath(__file__)).split(os.sep))
+        - os.path.dirname(os.path.abspath(__file__)).split(os.sep).index(
             'VeXtract'
         ) - 1
     )) + os.sep
@@ -14,10 +14,12 @@ log = logger.Logger(__name__)
 
 from pathlib import Path
 
-from analyzer.audio.audio_analyzer import AudioAnalyzer
+from analyzer.audio.audio import Audio
 
-AUDIO_PATH = Path(os.path.join(__root, "file", "te.wav"))
+AUDIO_PATH = Path(os.path.join(__root, "test", "test_file", "test_video.mp4"))
+
 
 def test_create_Audio():
-    audio = AudioAnalyzer(AUDIO_PATH)
+    audio = Audio(Path(AUDIO_PATH))
+
     print()
