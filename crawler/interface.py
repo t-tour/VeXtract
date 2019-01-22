366b22ed05dd410b8968fe51fd74207fecd3dff6
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2019-01-22 15:37:38 +0800

    crawler 架構

---
+++ crawler/interface.py
@@ -0,0 +1,38 @@
+import os
+import sys
+__root = os.path.abspath(
+    os.path.dirname(os.path.abspath(__file__)) + (os.sep + '..') * (
+        len(os.path.dirname(os.path.abspath(__file__)).split(os.sep))
+        - os.path.dirname(os.path.abspath(__file__)).split(os.sep).index(
+            'VeXtract'
+        ) - 1
+    )) + os.sep
+sys.path.append(__root)
+_ROOT = __root
+
+from helper import logger
+log = logger.Logger(__name__)
+
+from abc import abstractmethod, ABCMeta
+from typing import List
+from pathlib import Path
+
+from crawler.DTO import RealTimeComment, Comment, CrawlableInfo
+
+
+class Crawler(metaclass=ABCMeta):
+
+    @abstractmethod
+    def info_crawler(self, url) -> CrawlableInfo:
+        pass
+
+    @abstractmethod
+    def file_crawler(self, url) -> Path:
+        pass
+
+    @abstractmethod
+    def real_time_comments_crawler(self, url) -> List[RealTimeComment]:
+        pass
+
+    def comments_crawler(self, url) -> List[Comment]:
+        pass
